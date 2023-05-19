db9bed7cdacafd5a50ecf1d73fe371ca535efe57
marco76tv <marco.sottana@gmail.com>
2023-05-19 17:19:58 +0200

    up

--- Routes/web.php
+++ Routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('activity')->group(function () {
+/*Route::prefix('activity')->group(function () {
     Route::get('/', 'ActivityController@index');
 });
+*/
